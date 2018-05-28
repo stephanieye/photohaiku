e14b0a76de964d7ab4873dc56f8a59d058e6cde6
Stephanie Ye <stephanie.ye@gmail.com>
2018-05-28 19:10:54 +0100

    css

--- src/components/poems/Poem.js
+++ src/components/poems/Poem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Poem = ({ poem }) => {
   return (
@@ -8,13 +8,13 @@ const Poem = ({ poem }) => {
         style={{ backgroundImage: `url(${poem.image})` }}
       ></div>
       <div className="card-content">
-        <div className='columns is-mobile'>
-          <div className='column is-three-quarters'>
+        {/* <div className='columns is-mobile'> */}
+          {/* <div className='column is-three-quarters'> */}
             <h3>{poem.poet.username}</h3>
             <p className="subtitle is-6">{poem.createdAtRelative}</p>
-          </div>
-          <div className='column is-one-quarter has-text-right'>&#9734;</div>
-        </div>
+          {/* </div> */}
+          {/* <div className='column is-one-quarter has-text-right'>&#9734;</div> */}
+        {/* </div> */}
         <div>
           <p> {poem.haiku[0].line1} </p>
           <p> {poem.haiku[0].line2} </p>
